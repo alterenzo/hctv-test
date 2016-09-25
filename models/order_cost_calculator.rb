9bccf2ad79c0b69728e7eebb97d2a2698bf46253
Crescenzo D'Alterio <crescenzo.dalterio@gmail.com>
2016-09-25 20:21:42 +0100

    final cost method implemented

--- models/order_cost_calculator.rb
+++ models/order_cost_calculator.rb
@@ -5,21 +5,24 @@ class OrderCostCalculator
   DISCOUNT_PERCENT_ON_TOTAL_COST = 10
   DISCOUNT_ON_TOTAL_COST_TRESHOLD = 30
 
-  def final_cost(items)
-
+  def initialize(items)
+    @items = items
   end
 
+  def final_cost
+    (subtotal - express_deliveries_discount) * ((100 - discount_percent_on_final_cost) / 100.0)
+  end
 
-  def express_deliveries_discount(items)
-    if express_deliveries_count(items) >= EXPRESS_DELIVERY_TRESHOLD_FOR_DISCOUNT
-      express_deliveries_count(items) * EXPRESS_DELIVERY_DISCOUNT_AMOUNT
+  def express_deliveries_discount
+    if express_deliveries_count >= EXPRESS_DELIVERY_TRESHOLD_FOR_DISCOUNT
+      express_deliveries_count * EXPRESS_DELIVERY_DISCOUNT_AMOUNT
     else
       0
     end
   end
 
-  def discount_percent_on_final_cost(items)
-    if subtotal(items) > DISCOUNT_ON_TOTAL_COST_TRESHOLD
+  def discount_percent_on_final_cost
+    if discounted_subtotal > DISCOUNT_ON_TOTAL_COST_TRESHOLD
       DISCOUNT_PERCENT_ON_TOTAL_COST
     else
       0
@@ -28,15 +31,19 @@ class OrderCostCalculator
 
 private
 
-  def express_deliveries_count(items)
-    return 0 if items.empty?
-    items.select { |(_,delivery)| delivery.name == :express }.length
+  def express_deliveries_count
+    return 0 if @items.empty?
+    @items.select { |(_,delivery)| delivery.name == :express }.length
   end
 
-  def subtotal(items)
-    return 0 if items.empty?
-    items.reduce(0) { |sum, (_,delivery)| sum += delivery.price } - express_deliveries_discount(items)
+  def discounted_subtotal
+    return 0 if @items.empty?
+    @items.reduce(0) { |sum, (_,delivery)| sum += delivery.price } - express_deliveries_discount
   end
 
+  def subtotal
+    return 0 if @items.empty?
+    @items.reduce(0) { |sum, (_,delivery)| sum += delivery.price }
+  end
 
 end
--- spec/discount_calculator_spec.rb
+++ spec/discount_calculator_spec.rb
@@ -2,7 +2,6 @@ require './models/order_cost_calculator'
 
 describe OrderCostCalculator do
 
-  subject(:discount_calculator) { described_class.new }
   let(:express_delivery) { double :express_delivery, price: 20, name: :express }
   let(:standard_delivery) { double :standard_delivery, price: 10, name: :standard }
   let(:express_delivery_item) { [:broadcaster, express_delivery] }
@@ -13,8 +12,9 @@ describe OrderCostCalculator do
     context "more than #{described_class::EXPRESS_DELIVERY_TRESHOLD_FOR_DISCOUNT} express deliveries" do
       it "discounts #{described_class::EXPRESS_DELIVERY_DISCOUNT_AMOUNT} for each express delivery" do
         items = Array.new(2, express_delivery_item)
+        discount_calculator = described_class.new(items)
 
-        result = discount_calculator.express_deliveries_discount(items)
+        result = discount_calculator.express_deliveries_discount
 
         expect(result).to eq 2 * described_class::EXPRESS_DELIVERY_DISCOUNT_AMOUNT
       end
@@ -23,8 +23,9 @@ describe OrderCostCalculator do
     context "less than #{described_class::EXPRESS_DELIVERY_TRESHOLD_FOR_DISCOUNT} express deliveries" do
       it "should be zero" do
         items = [standard_delivery_item, express_delivery_item]
+        discount_calculator = described_class.new(items)
 
-        result = discount_calculator.express_deliveries_discount(items)
+        result = discount_calculator.express_deliveries_discount
 
         expect(result).to eq 0
       end
@@ -34,8 +35,9 @@ describe OrderCostCalculator do
       context "subtotal is more than #{described_class::DISCOUNT_ON_TOTAL_COST_TRESHOLD}" do
         it "should be #{described_class::DISCOUNT_PERCENT_ON_TOTAL_COST}%" do
           items = Array.new(3, express_delivery_item)
+          discount_calculator = described_class.new(items)
 
-          result = discount_calculator.discount_percent_on_final_cost(items)
+          result = discount_calculator.discount_percent_on_final_cost
 
           expect(result).to eq described_class::DISCOUNT_PERCENT_ON_TOTAL_COST
         end
@@ -44,17 +46,34 @@ describe OrderCostCalculator do
       context "subtotal is less or equal than #{described_class::DISCOUNT_ON_TOTAL_COST_TRESHOLD}" do
         it "should be 0%" do
           items = Array.new(2, express_delivery_item)
+          discount_calculator = described_class.new(items)
 
-          result = discount_calculator.discount_percent_on_final_cost(items)
+          result = discount_calculator.discount_percent_on_final_cost
 
           expect(result).to eq 0
         end
       end
     end
-
-
   end
 
+  describe '#final_cost' do
+    it 'returns the final discounted cost of a list of 3 express deliveries' do
+      items = Array.new(3, express_delivery_item)
+      discount_calculator = described_class.new(items)
 
+      result = discount_calculator.final_cost
 
+      expect(result).to eq 40.5
+    end
+
+    it 'reutrns the final discounted cost of a list of mixed deliveries' do
+      items = Array.new(3, standard_delivery_item)
+      items << express_delivery_item
+      discount_calculator = described_class.new(items)
+
+      result = discount_calculator.final_cost
+
+      expect(result).to eq 45
+    end
+  end
 end
