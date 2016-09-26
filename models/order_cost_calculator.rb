64f2123e4128e7f2cde5fe317b827320f209c02e
Crescenzo D'Alterio <crescenzo.dalterio@gmail.com>
2016-09-26 10:40:42 +0100

    express_delivery_discount now properly changes the price of every single express delivery

--- models/order_cost_calculator.rb
+++ models/order_cost_calculator.rb
@@ -3,7 +3,6 @@ class OrderCostCalculator
   attr_reader :items
 
   EXPRESS_DELIVERY_TRESHOLD_FOR_DISCOUNT = 2
-  EXPRESS_DELIVERY_DISCOUNT_AMOUNT = 5
   DISCOUNT_PERCENT_ON_TOTAL_COST = 10
   DISCOUNT_ON_TOTAL_COST_TRESHOLD = 30
   DISCOUNTED_EXPRESS_DELIVERY = 15
@@ -14,7 +13,7 @@ class OrderCostCalculator
 
   def final_cost
     express_deliveries_discount
-    subtotal * ((100.0 - discount_percent_on_final_cost) / 100 )
+    subtotal * ((100 - discount_percent_on_final_cost) / 100.0 )
   end
 
 
@@ -30,7 +29,7 @@ private
 
   def express_deliveries_discount
     if express_deliveries_count >= EXPRESS_DELIVERY_TRESHOLD_FOR_DISCOUNT
-      @items.map! do |(broadcaster, delivery)|
+      items.map! do |(broadcaster, delivery)|
         if delivery.name == :express
           delivery.name = :express_discounted
           delivery.price = DISCOUNTED_EXPRESS_DELIVERY
@@ -42,7 +41,7 @@ private
 
   def express_deliveries_count
     return 0 if @items.empty?
-    @items.select { |(_,delivery)| delivery.name == :express }.length
+    items.select { |(_,delivery)| delivery.name == :express }.length
   end
 
   def subtotal
--- spec/discount_calculator_spec.rb
+++ spec/discount_calculator_spec.rb
@@ -3,15 +3,17 @@ require './models/delivery'
 
 describe OrderCostCalculator do
 
-  let(:express_delivery) { double :express_delivery, price: 20, name: :express  }
+  let(:express_delivery) { double :express_delivery, :price => 20, :name => :express, :name= => nil, :price= => nil    }
   let(:standard_delivery) { double :standard_delivery, price: 10, name: :standard }
   let(:express_delivery_item) { [:broadcaster, express_delivery] }
   let(:standard_delivery_item) { [:broadcaster, standard_delivery] }
 
 
   describe '#final_cost' do
+
+
     it 'returns the final discounted cost of a list of 3 express deliveries' do
-      items = Array.new(3, Delivery.new(:express, 20))
+      items = Array.new(3, [:broadcaster,Delivery.new(:express, 20)])
       discount_calculator = described_class.new(items)
 
       result = discount_calculator.final_cost
@@ -20,8 +22,8 @@ describe OrderCostCalculator do
     end
 
     it 'reutrns the final discounted cost of a list of mixed deliveries' do
-      items = Array.new(3, Delivery.new(:express, 20))
-      items << Delivery.new(:standard, 10)
+      items = Array.new(3, [:broadcaster,Delivery.new(:standard, 10)])
+      items << [:broadcaster, Delivery.new(:express, 20)]
       discount_calculator = described_class.new(items)
 
       result = discount_calculator.final_cost
